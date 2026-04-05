01d1004d00708781b2026cc0e5677644888e7cf9
Olive-and-Vine <ben09099@gmail.com>
2026-04-05 13:49:58 +0800

    finish other use cases

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class PersonalBudgetTracker:
 
     def __init__(self):
         self.expenses = []
+        self.total = 0.0
         
     def add(self, date, amount, category, description):
         id = int(len(self.expenses) + 1)
@@ -25,11 +26,17 @@ class PersonalBudgetTracker:
         for expense in self.expenses:
             print(expense)
     
-    def filter_by_category():
-        return 'Expense filetered by category'
-    
-    def monthly_total():
-        return 'Expense monthly total'
+    def filter_by_category(self, filter_str:str):
+        filtered = [expense for expense in self.expenses if filter_str.lower() == expense.category.lower()]
+        for filter in filtered:
+            print(filter)
+    def monthly_total(self):
+        total = 0
+        for expense in self.expenses:
+            total += expense.amount
+        self.total = total
+        print(f'Monthly Total: {self.total}')
+
     
 
     
@@ -38,11 +45,10 @@ pbt = PersonalBudgetTracker()
 
 
 
-# •	"As Lena, I want to type a command to add an expense so I can log it in under 10 seconds."
+# •	"As Lena, I want to type a command to add an expense so I can log it in under 10 seconds." ok
 
 while True:
     command = input("Command: ")
-
     if command == 'add':
         date = input('Date: ')
         amount = float(input('Amount: '))
@@ -53,13 +59,19 @@ while True:
 
     elif command == 'list':
         pbt.show()
+
+    elif command == 'filter':
+        filter_str = str(input('Filter by: '))
+        pbt.filter_by_category(filter_str)
+    elif command == 'monthly-total':
+        pbt.monthly_total()
     elif command == 'exit':
         break
 
 
-# •	"As Lena, I want to see all expenses sorted by date so I know what I spent recently."
-# •	"As Lena, I want to filter by category (e.g., Food, Transport) so I can find specific items."
-# •	"As Lena, I want to see my monthly total so I know if I am over budget."
+# •	"As Lena, I want to see all expenses sorted by date so I know what I spent recently." ok
+# •	"As Lena, I want to filter by category (e.g., Food, Transport) so I can find specific items." ok
+# •	"As Lena, I want to see my monthly total so I know if I am over budget." ok
 
 
 
